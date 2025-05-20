712a07e625e71c970d9c59c1ca9ec6da7aa267ab
Sandeep Kumar Yadav <136022073+me-Sandeep-65@users.noreply.github.com>
2025-05-20 11:55:50 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -19,3 +19,23 @@ console.log(undefined < 0);
 // === 
 
 console.log("2" === 2);
+
+// ___________________________________________________________________________________________________________________________________________________
+
+/*
+eg.:-
+
+  1.  [] == false
+        [] → ToPrimitive → ""
+        false → 0, "" → 0
+        Final: 0 == 0 → true
+
+  2.  Rule Breakdown (from ECMAScript spec):
+        If one operand is a string and the other is a number → convert the string to a number.
+        If one operand is a boolean → convert the boolean to a number (true → 1, false → 0).
+        If both are objects → try to ToPrimitive and then compare.
+        If one is null and the other is undefined → true.
+        Otherwise → false.
+
+
+*/
